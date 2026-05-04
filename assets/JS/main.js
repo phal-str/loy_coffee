54234d33767a193d8ba9da89a8d87f9c31cbc602
sok phal <sokphal@PAPILOKO.local>
2026-05-04 19:34:47 +0700

    push

---
+++ assets/JS/main.js
@@ -0,0 +1,16 @@
+AOS.init({
+    // duration: 1000,
+    // once: true 
+});
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+});
+
